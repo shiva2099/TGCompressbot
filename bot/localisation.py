eca22213ff9a40ac0c7faa3627f93336d4fcca51
pRiyaNshu bHaRdwaj <itispriyanshu@gmail.com>
2021-01-02 01:58:15 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -1,41 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) PublicLeech Author(s)
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from bot.get_cfg import get_config
 
 
 class Localisation:
-    START_TEXT = "Hello My master @priyanshu_bhardwaj jee , \n\nThis is  Telegram Video Compresser Bot😊  \n\n<b>Please send me any Telegram Video or file . I Will compress the file to your desired Size .</b> \n\nclick /help for more details... \n\nChannel  :@about_bhardwajji"
+    START_TEXT = "Hello, \n\nThis is a Telegram Video Compress Bot \n\n<b>Please send me any Telegram big file I Will compress a small file</b> \n\n/help for more details. \n\nWorking Group: @CompressZone"
    
     ABS_TEXT = " Please don't be selfish."
     
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     
     
-    DOWNLOAD_START = "📥 Downloading your file my master 📥 \n"
+    DOWNLOAD_START = "📥 Downloading 📥 \n"
     
-    UPLOAD_START = "📤 Uploading your File my master 📤 \n"
+    UPLOAD_START = "📤 Uploading 📤 \n"
     
-    COMPRESS_START = "📀 Trying to compress... 📀"
+    COMPRESS_START = "📀 Trying to compress ... 📀"
     
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95gb due to Telegram API limitations."
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
     
-    COMPRESS_SUCCESS = "📥 Downloaded in {}\n\n📀 Compressed in {}\n\n📤 Uploaded in {}"
+    COMPRESS_SUCCESS = "📥 Downloaded in {}\n\n📀 Compressed in {}\n\n📤 Uploaded in {}\n\nBy @CompressZone"
 
     COMPRESS_PROGRESS = "⏳ ETA: {}\n🚀 Progress: {}%"
 
@@ -55,11 +42,11 @@ class Localisation:
     
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     
-    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "⚠️ Already One Process going on. \n or \n A media already exists. \n  Please send /cancel to delete existing media. ⚠️"
+    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "⚠️ Already One Process going on. \nor, \nA media already exists. \nSend /cancel to delete existing media. ⚠️"
     
     HELP_MESSAGE = get_config(
         "STRINGS_HELP_MESSAGE",
-        "Hi am Video Compressor Bot \n\n1. Send your telegram video file \n2. Reply the file - /compress And Percentage \nEg:- <code>/compress 50</code> \n\nSupport @priyanshu_bhardwaj"
+        "Hi, I am Video Compressor Bot .  \n\n1. Send me your telegram big video file \n2. Reply the file - /compress And Percentage \nEg: <code>/compress 50</code> \n\nSupport Group: @CompressZone"
     )
     WRONG_MESSAGE = get_config(
         "STRINGS_WRONG_MESSAGE",
