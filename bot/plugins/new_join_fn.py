4883793a8ecc93b79f211f9ffefd13a244ca241a
pRiyaNshu bHaRdwaj <itispriyanshu@gmail.com>
2021-01-02 02:19:55 +0530

    Update new_join_fn.py

--- bot/plugins/new_join_fn.py
+++ bot/plugins/new_join_fn.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
+# (c) Shrimadhav U K | @priyanshu_bhardwaj
 
 
 from bot.localisation import Localisation
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 async def new_join_f(client, message):
     # delete all other messages, except for AUTH_USERS
@@ -25,5 +26,15 @@ async def help_message_f(client, message):
     # display the /help message
     await message.reply_text(
         Localisation.HELP_MESSAGE,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('My Father 🖤 ', url='https://telegram.me/priyanshu_bhardwaj')
+                ],
+                [
+                    InlineKeyboardButton('Source 🌟', url='https://github.com/bhardwajjEE/TGCompressbot')
+                ]
+            ]
+        ),
         quote=True
     )
